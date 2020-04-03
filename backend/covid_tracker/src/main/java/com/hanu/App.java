f1018d3e19a0836fbe2fd993f35379b2c065911b
binhdoitsme <binhdoitsme@gmail.com>
2020-04-03 21:33:27 +0700

    Created backend project structure.

---
+++ backend/covid_tracker/src/main/java/com/hanu/App.java
@@ -0,0 +1,14 @@
+package com.hanu;
+
+public final class App implements Runnable {
+    private App() {
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new App().run();
+    }
+}
