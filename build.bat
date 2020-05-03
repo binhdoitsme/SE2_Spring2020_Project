3b4adbf918bcf3ae605419263d096c43e914edcf
binhdoitsme <binhdoitsme@gmail.com>
2020-05-03 15:46:42 +0700

    Merge branch 'quynhtn' of https://github.com/binhdoitsme/SE2_Spring2020_Project into HEAD
    # Conflicts:
    #	backend/covid_tracker/src/main/java/com/hanu/base/Repository.java
    #	backend/covid_tracker/src/main/java/com/hanu/controller/RecordController.java
    #	backend/covid_tracker/src/main/java/com/hanu/db/RecordRepositoryImpl.java
    #	backend/covid_tracker/src/main/java/com/hanu/db/UserRepositoryImpl.java
    #	backend/covid_tracker/src/main/java/com/hanu/domain/model/Record.java
    #	backend/covid_tracker/src/main/java/com/hanu/domain/repository/RecordRepository.java
    #	backend/covid_tracker/src/main/java/com/hanu/servlet/RecordServlet.java
    #	backend/covid_tracker/src/test/java/com/hanu/mock/MockRecordRepository.java

---
+++ build.bat
@@ -0,0 +1,4 @@
+cd /D "%~dp0"
+./backend/covid_tracker/mvn_build.bat
+cd ./frontend/covid_tracker
+npm install
---
+++ run.bat
@@ -0,0 +1,3 @@
+cd /D "%~dp0"
+start nodemon ./frontend/covid_tracker/app.js
+./backend/covid_tracker/target/server.bat
